95d3dca2061eca977ddb5c6cd781ca441b1e09e6
owen <owardlaw@me.com>
2022-07-07 16:18:41 -0700

    Flask app creation

--- detection.py
+++ detection.py
@@ -2,7 +2,6 @@ import cv2
 import os
 import numpy as np
 import json
-import matplotlib.pyplot as plt
 from detectron2.structures import BoxMode
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2 import model_zoo
@@ -11,14 +10,8 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_test_loader
-import numpy as np
-import os, json, cv2
-from PIL import Image as im
-import numpy as np
 
-
-
-# json format
+# Reading Json Data
 def get_data_dicts(directory, classes):
     dataset_dicts = []
     for filename in [file for file in os.listdir(directory) if file.endswith('.json')]:
@@ -28,15 +21,14 @@ def get_data_dicts(directory, classes):
 
         record = {}
         filename = os.path.join(directory, img_anns["imagePath"])
-        record["file_name"] = filename
-
-        # Pixelsegmentation      
+        
+        record["file_name"] = filename    
         annos = img_anns["shapes"]
         objs = []
         for anno in annos:
-            px = [a[0] for a in anno['points']] # X-coord
-            py = [a[1] for a in anno['points']] # Y-coord
-            poly = [(x, y) for x, y in zip(px, py)] # Poly for segmentation
+            px = [a[0] for a in anno['points']] # x coord
+            py = [a[1] for a in anno['points']] # y-coord
+            poly = [(x, y) for x, y in zip(px, py)] # poly for segmentation
             poly = [p for x in poly for p in x]
 
             obj = {
@@ -44,7 +36,8 @@ def get_data_dicts(directory, classes):
                 "bbox_mode": BoxMode.XYXY_ABS,
                 "segmentation": [poly],
                 "category_id":0,
-                "category_id": classes.index(anno['label']),
+
+                # "category_id": classes.index(anno['label']),
                 "iscrowd": 0
             }
             objs.append(obj)
@@ -53,10 +46,8 @@ def get_data_dicts(directory, classes):
     return dataset_dicts
 
 
-# Define class/classes
+# Define class from labels and dir where test and train is stored 
 classes = ['stop_sign']
-
-# Path to train and test data
 data_path = './data/'
 
 for d in ["train", "test"]:
@@ -64,45 +55,40 @@ for d in ["train", "test"]:
         "category_" + d, 
         lambda d=d: get_data_dicts(data_path+d, classes)
     )
+    
     MetadataCatalog.get("category_" + d).set(thing_classes=classes)
 
 microcontroller_metadata = MetadataCatalog.get("category_train")
 
-# Model  
+# Model values, increase "cfg.SOLVER.MAX_ITER" for more training 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 cfg.DATASETS.TRAIN = ("category_train",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-
-#Config model values
-cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.IMS_PER_BATCH = 4
 cfg.SOLVER.BASE_LR = 0.00025
 cfg.SOLVER.MAX_ITER = 1000
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
 
-#Load trainer
+# Create dir for model
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg) 
 trainer.resume_or_load(resume=False)
 
 #Train model (comment out/in)
-if input("Train: y/n?") == "y":
-    trainer.train()
+# trainer.train()
 
-else:
-    None
-
-# save model
+# Save model
 cfg.OUTPUT_DIR = "./output/"
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  #Set the testing threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9  # set the testing threshold for this model
 cfg.DATASETS.TEST = ("skin_test", )
 predictor = DefaultPredictor(cfg)
 
-#image functions
-def detection(frame):   
+# Image detection functions
+def detect_signs(frame):   
     outputs = predictor(frame)
     v = Visualizer(frame[:, :, ::-1],
                        metadata=microcontroller_metadata, 
@@ -147,29 +133,29 @@ def detection(frame):
         color = (200,200,200)
         img_mask[np.where((array_img==[255,255,255]).all(axis=2))] = color
         
-
     # Copies of images to combine overlay of detection pixels
     overlay = frame.copy()
     output = frame.copy()
-
-    pred_nums = len(scores)
-    print(f"Number of stop signs = {pred_nums}")
 
     #draw box 
     img_mask = np.asarray(img_mask)
     output = cv2.addWeighted(img_mask, 1, overlay, 1, 0) 
     return output, scores, mask_array, white_cords
 
-# Choose file from "test_imgs"
-filename = "test1.jpg"
-img = cv2.imread("./test_imgs/"+filename)
+# Test from detection file (comment in/out below)
 
-# Run detection 
-detection = detection(img)
+# # Choose file from "test_imgs"
+# filename = "test1.jpg"
+# img = cv2.imread("./test_imgs/"+filename)
 
-# Show, write img
-cv2.imshow("img", detection[0])
-cv2.imwrite("detection.jpg", detection[0])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# # Run detection 
+# detection = detection(img)
+
+# output = detection[0]
+
+# # Show, write img
+# cv2.imshow("img", output)
+# cv2.imwrite("detection.jpg", output)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
  
---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+from torch import det
+from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from detection import detect_signs, dir_to_img
+from show_img import show
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'PNG'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        img = cv2.imread('./static/uploads/' + filename)
+        height = img.shape[0]
+        width = img.shape[1]
+        ratio = (width/height) * 480
+        img = cv2.resize(img, (int(ratio), 480), interpolation = cv2.INTER_AREA)
+
+        detection = detect_signs(img)
+        cv2.imwrite('./static/uploads/' + filename, detection[0])
+
+        if len(detection[3]) == 1:
+            dect = " sign."
+            
+        else:
+            dect = " signs."
+
+        flash(f'Found {len(detection[3])} {dect}')
+        return render_template('upload.html', filename=filename)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == "__main__":
+    app.run()
